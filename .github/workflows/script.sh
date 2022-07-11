3b3e597ffccd82030c9ef06968d814de0a1fc00c
rkrupesh44 <88672591+rkrupesh44@users.noreply.github.com>
2022-07-11 15:55:41 +0530

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,3 +1,3 @@
-curl -X POST -H "Accept: application/vnd.github.v3+json" -u rkrupesh44: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 \
+curl -X POST -H "Accept: application/vnd.github.v3+json" -u rkrupesh44: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 \
 https://api.github.com/repos/rkrupesh44/Final_GitHub_Actions/actions/workflows/sample.yml/dispatches \
 -d '{"ref":"ref"}'
